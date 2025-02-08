bc4eb70a7717782bb7522aed83fcf53f57fec74d
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-02-08 14:56:32 +0000

    Add deploy script and test smart contract

---
+++ contracts/deploy.mjs
@@ -0,0 +1,39 @@
+import { Contract, ContractFactory, ethers } from 'ethers';
+import { readFileSync } from "fs";
+import solc from "solc";
+
+const source = readFileSync("./Test.sol").toString();
+
+const input = {
+	language: "Solidity",
+	sources: {
+		"Test.sol": {
+			content: source
+		}
+	},
+	settings: {
+		outputSelection: {
+			"*": {
+				"*": ["*"]
+			}
+		}
+	}
+}
+
+console.log("Compiling contract...");
+const bin = JSON.parse(solc.compile(JSON.stringify(input)));
+console.log("Finished compiling.");
+
+const ABI = bin.contracts["Test.sol"]["Test"].abi;
+const bytecode = bin.contracts["Test.sol"]["Test"].evm.bytecode.object;
+
+console.log("Deploying to network...");
+
+const key = readFileSync("./.key").toString().trim();
+
+const provider = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545/");
+const signer = new ethers.Wallet(key, provider);
+
+const factory = new ContractFactory(ABI, bytecode, signer);
+const contract = await factory.deploy();
+console.log(contract);
