17470976c19ab4f1d9c97c06ceb26e986d52baa5
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-02-08 17:53:26 +0000

    client works

--- frontend/src/greenfield/greenfieldClient.ts
+++ frontend/src/greenfield/greenfieldClient.ts
@@ -2,8 +2,8 @@ import { Client } from "@bnb-chain/greenfield-js-sdk";
 
 // Create a Greenfield client instance
 export const client = Client.create(
-	"https://gnfd-testnet-fullnode-tendermint-ap.bnbchain.org", // Greenfield Testnet RPC endpoint
-	"5600" // Testnet chain ID
+	"https://gnfd-testnet-fullnode-tendermint-us.bnbchain.org", // Greenfield Testnet RPC endpoint
+	"greenfield_5600-1" // Testnet chain ID
 );
 
 console.log(client);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import tailwindcss from "@tailwindcss/vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-	plugins: [solidPlugin(), tailwindcss()],
+	plugins: [solidPlugin(), tailwindcss(), nodePolyfills()],
 	server: {
 		port: 3000,
 	},
