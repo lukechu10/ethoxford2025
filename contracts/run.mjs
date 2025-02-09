30c25a517a830755d311bc3e42c6bd3eda8cfb37
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2025-02-09 10:50:51 +0000

    fmt

--- contracts/run.mjs
+++ contracts/run.mjs
@@ -1,6 +1,6 @@
 // Utility script to compile and deploy the contract to the BSC Testnet.
 
-import { ContractFactory, ethers } from 'ethers';
+import { ContractFactory, ethers } from "ethers";
 import { readFileSync, writeFileSync } from "fs";
 import solc from "solc";
 
@@ -11,17 +11,17 @@ function compile_solc() {
 		language: "Solidity",
 		sources: {
 			"DeSci.sol": {
-				content: source
-			}
+				content: source,
+			},
 		},
 		settings: {
 			outputSelection: {
 				"*": {
-					"*": ["*"]
-				}
-			}
-		}
-	}
+					"*": ["*"],
+				},
+			},
+		},
+	};
 
 	console.log("Compiling contract...");
 	const bin = JSON.parse(solc.compile(JSON.stringify(input)));
@@ -36,23 +36,26 @@ function compile_solc() {
 const { ABI, bytecode } = compile_solc();
 writeFileSync("./abi.json", JSON.stringify(ABI));
 
-const deploy = process.argv.find(arg => arg === "--deploy");
+const deploy = process.argv.find((arg) => arg === "--deploy");
 
 if (deploy) {
 	console.log("Deploying to network...");
 
 	const key = readFileSync("./.key").toString().trim();
 
-	const provider = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545/");
+	const provider = new ethers.JsonRpcProvider(
+		"https://data-seed-prebsc-1-s1.binance.org:8545/",
+	);
 	const signer = new ethers.Wallet(key, provider);
 
 	const factory = new ContractFactory(ABI, bytecode, signer);
 	const contract = await factory.deploy();
 
-
 	const address = await contract.getAddress();
 	writeFileSync("./address.json", JSON.stringify({ address }));
-	console.log("Contract successfully deployed. Address: " + address)
+	console.log("Contract successfully deployed. Address: " + address);
 } else {
-	console.log("Contract was not deployed. Pass the --deploy flag to deploy the contract.");
+	console.log(
+		"Contract was not deployed. Pass the --deploy flag to deploy the contract.",
+	);
 }
